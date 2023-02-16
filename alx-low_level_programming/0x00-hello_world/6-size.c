3dec5e70cd8ce14345aa30e7c0ea33cf3d27c254
Woollams <rwoollams2@gmail.com>
2023-02-16 15:34:31 -0800

    wins

---
+++ alx-low_level_programming/0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that prints the size of various types of bytes
+ *
+ * Return: Always return 0.
+*/
+int main(void)
+{
+	int a;
+	float b;
+	char c;
+	long int d;
+	long long int e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
